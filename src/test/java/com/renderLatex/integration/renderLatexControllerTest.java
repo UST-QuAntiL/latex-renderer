ccdea9c05fb60f6d3ee314bc93f7a913371a6d2b
Martin Beisel <beiselmn@gmail.com>
2021-05-27 10:19:29 +0200

    Fix for junit-vintage engine

--- src/test/java/com/renderLatex/integration/renderLatexControllerTest.java
+++ src/test/java/com/renderLatex/integration/renderLatexControllerTest.java
@@ -5,12 +5,10 @@ import com.renderLatex.entities.LatexContent;
 import com.renderLatex.util.IntegrationTestHelper;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -21,7 +19,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class renderLatexControllerTest {
